1e0e3d5424252631b2d594f18dc2b5a9e9ab5ca7
valen <valenmat24@hotmail.com>
2019-06-07 13:08:35 -0300

    puse los tres carruceles, queda darles diseño

--- proyectointegrador.js
+++ proyectointegrador.js
@@ -1,8 +1,7 @@
-var key = '4e07555aebd06c43dcbce0e5a4e01f05'
-
+var key = '14227022544b0f4bb15b6a738161d32f'
 window.onload = function () {
 
-
+//carrusel1
   fetch("https://api.themoviedb.org/3/movie/popular?api_key="+key+"&language=en-US&page=1")
 
   .then (function(response) {
@@ -17,6 +16,8 @@ window.onload = function () {
     var title
     var li
     var ul = document.querySelector("section.populares ul")
+    //var ul1 = document.querySelector("section.MayorPuntaje ul")
+    //var ul2 = document.querySelector("section.comingSoon ul")
     for (var i = 0; i < 5; i++) {
        poster = arrayDePeliculas[i].poster_path
        title = arrayDePeliculas[i].title
@@ -42,5 +43,90 @@ window.onload = function () {
      .catch(function(error){
         console.log("There was na error: " + error);
       })
+
+  //carrousel2
+  fetch("https://api.themoviedb.org/3/movie/top_rated?api_key="+key+"&language=en-US&page=1")
+
+      .then (function(response) {
+        return response.json();
+      })
+      .then (function(information){
+        console.log(information);
+        console.log(information.results);
+        var arrayDePeliculas = information.results
+        var urlImg = "https://image.tmdb.org/t/p/original"
+        var poster
+        var title
+        var li
+        //var ul = document.querySelector("section.populares ul")
+        var ul1 = document.querySelector("section.MayorPuntaje ul")
+        //var ul2 = document.querySelector("section.comingSoon ul")
+        for (var i = 0; i < 5; i++) {
+           poster = arrayDePeliculas[i].poster_path
+           title = arrayDePeliculas[i].title
+           console.log(arrayDePeliculas[i].title)
+      //carrusel
+           li = "<li class='mySlides2'>"
+             li += '<h1>'+ title +'</h1>'
+             li += '<img src='+ urlImg + poster + '>'
+           li += '</li>'
+           ul1.innerHTML += li
+          //console.log(poster);
+        }
+
+
+        //esto deja visible, solo el primier LI, del PRIMER carrousel
+        showSlides(1, 1);
+        //esto deja visible, solo el primer LI, del SEGUNDO carrousel
+        // showSlides(1, 1);
+        //esto deja visible, solo el primer LI, del SEGUNDO carrousel
+        // showSlides(1, 2);
+
+        })
+         .catch(function(error){
+            console.log("There was na error: " + error);
+          })
+  //carrusel3
+    fetch("https://api.themoviedb.org/3/movie/upcoming?api_key="+key+"&language=en-US&page=1")
+
+          .then (function(response) {
+            return response.json();
+          })
+          .then (function(information){
+            console.log(information);
+            console.log(information.results);
+            var arrayDePeliculas = information.results
+            var urlImg = "https://image.tmdb.org/t/p/original"
+            var poster
+            var title
+            var li
+            //var ul = document.querySelector("section.populares ul")
+            //var ul1 = document.querySelector("section.MayorPuntaje ul")
+            var ul2 = document.querySelector("section.comingSoon ul")
+            for (var i = 0; i < 5; i++) {
+               poster = arrayDePeliculas[i].poster_path
+               title = arrayDePeliculas[i].title
+               console.log(arrayDePeliculas[i].title)
+          //carrusel
+               li = "<li class='mySlides3'>"
+                 li += '<h1>'+ title +'</h1>'
+                 li += '<img src='+ urlImg + poster + '>'
+               li += '</li>'
+               ul2.innerHTML += li
+              //console.log(poster)
+            }
+
+
+            //esto deja visible, solo el primier LI, del PRIMER carrousel
+            showSlides(1, 2);
+            //esto deja visible, solo el primer LI, del SEGUNDO carrousel
+            // showSlides(1, 1);
+            //esto deja visible, solo el primer LI, del SEGUNDO carrousel
+            // showSlides(1, 2);
+
+            })
+             .catch(function(error){
+                console.log("There was na error: " + error);
+              })
 
 }
